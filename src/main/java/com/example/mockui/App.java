f61b15c4f4de6b65f3c63bc8e1dbb403a45ff53e
1cgul <cgulino03@gmail.com>
2023-10-24 02:45:00 -0400

    .

--- src/main/java/com/example/mockui/App.java
+++ src/main/java/com/example/mockui/App.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main-screen.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("login-screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("SmartStacks!");
         stage.setScene(scene);
